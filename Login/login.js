ecd3b7c1b9233d2a8916713188dbdc7d8fd3c247
Aman <amanagrawal581@gmail.com>
2023-09-10 18:44:44 +0530

    login fronted ready

---
+++ Login/login.js
@@ -0,0 +1,19 @@
+async function login(event) {
+    try{
+        event.preventDefault();
+        const obj = {
+            email:event.target.email.value,
+            password:event.target.password.value
+        }
+     const response= await axios.post('http://localhost:3000/user/login',obj)
+        if(response.status=201){
+             alert("User Succesfully logged in")
+        }
+        else{
+            throw new Error('failed to login')
+         }
+        }
+    catch(err){
+        document.body.innerHTML=`<div style="color:red;">${err}</div>`;
+    }
+}

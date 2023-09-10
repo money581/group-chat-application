f87502ff5fed943d8c34fc3babed244836c53e81
Aman <amanagrawal581@gmail.com>
2023-09-10 15:52:55 +0530

    fronted ready

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const sequelize = require('./util/database');
+const cors = require('cors');
+const app = express();
+dotenv.config();
+app.use(cors())
+app.use(bodyParser.json({ extended: false }));
+//const userRoute = require('./routers/user');
+//app.use(userRoute)
+sequelize
+    .sync()
+    //.sync({force: true})
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    }); 
+
+
+
